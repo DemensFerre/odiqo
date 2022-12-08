d550808a35d22c02d4b0edac4bd62ac5623d631b
Max <87299631+DemensFerre@users.noreply.github.com>
2022-12-08 23:21:50 +0300

    added full block videos

--- js/main.js
+++ js/main.js
@@ -8,4 +8,24 @@ const headerScroll = () => {
     }
   });
 };
+
+const videosPlay = () => {
+  const play = document.querySelector(".videos__play");
+
+  play.addEventListener("click", () => {
+    const videoItem = document.querySelector(".videos__item");
+
+    videoItem.insertAdjacentHTML(
+      "beforeend",
+      '<iframe \
+      src="https://www.youtube.com/embed/CxuiFNYnEr4" \
+      title="YouTube video player" \
+      frameborder="0" \
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media;  gyroscope; picture-in-picture" \
+      allowfullscreen class="videos__video"></iframe>'
+    );
+  });
+};
+
 headerScroll();
+videosPlay();
