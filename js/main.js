731b9f87d45184e6887f465117fec13ef2fefaee
Max <87299631+DemensFerre@users.noreply.github.com>
2022-12-11 21:57:45 +0300

    added burger menu for header

--- js/main.js
+++ js/main.js
@@ -27,5 +27,18 @@ const videosPlay = () => {
   });
 };
 
+const burgerOpen = () => {
+  const burger = document.querySelector(".header__burger");
+  const menu = document.querySelector(".header__nav");
+  const menuClose = document.querySelector(".header__nav-close");
+  burger.addEventListener("click", () => {
+    menu.classList.add("header__nav_active");
+  });
+  menuClose.addEventListener("click", () => {
+    menu.classList.remove("header__nav_active");
+  });
+};
+
 headerScroll();
 videosPlay();
+burgerOpen();
