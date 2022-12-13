a57fc0fb6b44b393999544005550ebb9f24d4f67
Max <87299631+DemensFerre@users.noreply.github.com>
2022-12-13 19:08:43 +0300

    added styles for site adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const headerScroll = () => {
   const header = document.querySelector(".header");
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
+    if (window.scrollY >= 100) {
       header.classList.add("header_active");
     } else {
       header.classList.remove("header_active");
